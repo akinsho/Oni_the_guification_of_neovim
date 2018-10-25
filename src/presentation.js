ab8aee348423463d45fc9f47c726daae1babfd8c
Akin909 <akin.sowemimo@gmail.com>
2018-10-25 11:53:31 +0100

    flesh out slides there are gonna be alot of commits like this specifically added section on terminal limitations and Oni as an IDE

--- src/presentation.js
+++ src/presentation.js
@@ -47,8 +47,8 @@ export default class Presentation extends React.Component {
                     </Text>
                     <Notes>
                         <p>
-                            Introduce myself the project, explain that I'm a collaborator. Mention
-                            the others i.e. Bryan, Manuel, Ryan and Tal
+                            Introduce myself the project, explain that I'm a collaborator. Shout out
+                            to oni's awesome maintainer, collaborators and community.
                         </p>
                     </Notes>
                 </Slide>
@@ -72,16 +72,37 @@ export default class Presentation extends React.Component {
                         What is Oni?
                     </Heading>
                     <Text margin="10px 0 0" size={6} textColor="secondary" textAlign="left">
-                        Oni is first and foremost a GUI is for Neovim, which aims to provide users
-                        with all the power of modal editing without the limitations of a terminal
-                        UI.
+                        Oni is a GUI (Graphical User Interface) for Neovim, which aims to provide
+                        users with all the power of modal editing without the limitations of a
+                        terminal UI.
                     </Text>
+                    <Notes>
+                        <p>
+                            Discuss modal editing in brief (its vim conf so don't need to sell this
+                            too hard). However vim has classically been bound to the terminal
+                            although with the advent of neovim which can be embedded inside other
+                            programs
+                        </p>
+                    </Notes>
                 </Slide>
                 <Slide>
                     <Text margin="10px 0 0" size={6} textColor="secondary" textAlign="left">
                         Further Oni aims to provide Neovim with the features of an IDE such as
                         integration with language servers, more advanced syntax highlighting
                     </Text>
+                    <Notes>
+                        <p>
+                            Its now possible to create an IDE with neovim at its core that provides
+                            the power and speed of modal editing combined with the nicieites and
+                            utilties of a GUI IDE.{" "}
+                        </p>
+                        <p>
+                            A key difference between Oni and alternatives like vscode or sublime
+                            with a vim plugin is that at its core Oni is just vim, that means it
+                            uses your vim config with all your carefully crafted mappings and
+                            scripts and you can use all of your plugins as normal
+                        </p>
+                    </Notes>
                 </Slide>
                 <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
                     <BlockQuote>
