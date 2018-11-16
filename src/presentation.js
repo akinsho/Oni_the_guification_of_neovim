eeb5c436b1561a02e8004387ff4275e68f4c9a7b
Akin909 <akin.sowemimo@gmail.com>
2018-11-16 23:13:59 +0000

    Add team pics to slides and improve content [WIP]

--- src/presentation.js
+++ src/presentation.js
@@ -3,13 +3,13 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
+  Layout,
+  Fill,
+  Image,
   Deck,
   Heading,
   ListItem,
   List,
-  Quote,
   Slide,
   Text,
   Notes
@@ -18,18 +18,23 @@ import {
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
+// import team pics
+import bryan from "./images/bryan.png";
+import ryan from "./images/ryan.png";
+import manuel from "./images/manuel.jpg";
+
 // Require CSS
 require("normalize.css");
 
 const theme = createTheme(
   {
-    primary: "white",
-    secondary: "#1F2022",
-    tertiary: "#03A9FC",
+    primary: "#03A9FC",
+    secondary: "white",
+    tertiary: "#1F2022",
     quartenary: "#CECECE"
   },
   {
-    primary: "Montserrat",
+    primary: "Fira Code",
     secondary: "Helvetica"
   }
 );
@@ -42,24 +47,49 @@ export default class Presentation extends React.Component {
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+        <Slide transition={["zoom"]}>
+          <Heading size={1} fit caps lineHeight={1}>
             Oni: The GUI-fication of Neovim
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={6}>
+          <Text margin="10px 0 0" textColor="secondary" size={6}>
             Akin Sowemimo
           </Text>
           <Notes>
             <p>
               {`Introduce myself the project, explain that I'm a collaborator. Shout out
-                            to oni's awesome maintainer, collaborators and community.`}
+                to oni's awesome maintainer, collaborators and community.`}
             </p>
           </Notes>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Talking Points
-          </Heading>
+        <Slide>
+          <p>Hi 👋🏾</p>
+          <p>Software Developer @Monzo</p>
+          <p>Twitter -> @Akin_So</p>
+        </Slide>
+        <Slide textColor="tertiary">
+          <Heading size={6}>The Team</Heading>
+          <Layout>
+            <Fill>
+              <figure style={{ display: "inline-block" }}>
+                <Image height="200px" width="250" src={bryan} />
+                <figcaption>Bryan</figcaption>
+              </figure>
+              <figure style={{ display: "inline-block" }}>
+                <Image height="200px" width="250" src={ryan} />
+                <figcaption>Ryan</figcaption>
+              </figure>
+              <figure style={{ display: "inline-block" }}>
+                <Image height="200px" width="250" src={manuel} />
+                <figcaption>Manuel</figcaption>
+              </figure>
+              <figure style={{ display: "inline-block" }}>
+                <figcaption>Tal</figcaption>
+              </figure>
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading size={6}>Talking Points</Heading>
           <List>
             <ListItem>What is Oni?</ListItem>
             <ListItem>Who is Oni for?</ListItem>
@@ -71,19 +101,14 @@ export default class Presentation extends React.Component {
             <p>Brief overview of what we are going to be talking about</p>
           </Notes>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            What is Oni?
-          </Heading>
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
-            Oni is a GUI (Graphical User Interface) for Neovim, which aims to
-            provide users with all the power of modal editing without the
-            limitations of a terminal UI.
+        <Slide transition={["fade"]}>
+          <Heading size={6}>What is Oni?</Heading>
+          <Text margin="10px  0" size={6} textAlign="left">
+            Oni is a Graphical User Interface(GUI) for Neovim.
+          </Text>
+          <Text margin="20px 0 0" size={6} textAlign="left">
+            It allows users to have all the power of modal editing without the
+            limitations of a terminal UI
           </Text>
           <Notes>
             <p>
@@ -95,15 +120,10 @@ export default class Presentation extends React.Component {
           </Notes>
         </Slide>
         <Slide>
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
-            Further Oni aims to provide Neovim with the features of an IDE such
-            as integration with language servers, more advanced syntax
-            highlighting
+          <Text margin="10px 0 0" size={6} textAlign="left">
+            But Oni does more than that. Oni allows Neovim to have features of
+            an IDE such as integration with language servers, more advanced
+            syntax highlighting
           </Text>
           <Notes>
             <p>
@@ -120,15 +140,8 @@ export default class Presentation extends React.Component {
           </Notes>
         </Slide>
         <Slide>
-          <Heading size={6} textColor="primary" caps>
-            Who is Oni for?
-          </Heading>
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
+          <Heading size={6}>Who is Oni for?</Heading>
+          <Text margin="10px 0 0" size={6} textAlign="left">
             Oni - aims to expand on the core vim experience by providing
             additional features and an enhanced GUI we try to ensure that as
             many features as possible can be toggled or configured.
@@ -140,13 +153,8 @@ export default class Presentation extends React.Component {
             provide the right experience.
           </Notes>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
+        <Slide transition={["fade"]}>
+          <Text margin="10px 0 0" size={6} textAlign="left">
             The aim is also to lessen the amount of time and effort that goes
             into setting up vim (or at least of the visual aspects of setting up
             vim). We see oni as being a good choice for vim users who want the
@@ -154,16 +162,9 @@ export default class Presentation extends React.Component {
             fully fledged modal editor
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <Heading size={6} textColor="primary" caps>
-            Oni's Architecture
-          </Heading>
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
+        <Slide transition={["fade"]}>
+          <Heading size={6}>Oni's Architecture</Heading>
+          <Text margin="10px 0 0" size={6} textAlign="left">
             Oni is an electron app, written with React, Typescript
             commnunication with Neovim via the msgpack API.
           </Text>
@@ -174,43 +175,35 @@ export default class Presentation extends React.Component {
           </Notes>
         </Slide>
         <Slide>
-          <Heading size={6} textColor="primary" caps>
-            Where are we now?
-          </Heading>
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
-            Some of Oni's most recently implemented features include -
-          </Text>
-          <List>
-            <ListItem size={6}>
-              Improved Syntax highlighting - based on textmate
-            </ListItem>
-            <ListItem size={6}>
-              File explorer - which prioritises navigation via the keyboard
-              using, vim like mappings
-            </ListItem>
-            <ListItem size={6}>
-              VCS Integration - currently only Git is supported but the
-              framework exists to add more vcs
-            </ListItem>
-            <ListItem size={6}>
-              Achievements - This functionality is aimed at getting new users
-              interested in learning to use vim
-            </ListItem>
-          </List>
+          <Heading size={6}>Where are we now?</Heading>
+          <Layout>
+            <Fill>
+              <Text size={6} textAlign="left">
+                Some of Oni's most recently implemented features include -
+              </Text>
+              <List>
+                <ListItem size={6}>
+                  Improved Syntax highlighting - based on textmate
+                </ListItem>
+                <ListItem size={6}>
+                  File explorer - which prioritises navigation via the keyboard
+                  using, vim like mappings
+                </ListItem>
+                <ListItem size={6}>
+                  VCS Integration - currently only Git is supported but the
+                  framework exists to add more vcs
+                </ListItem>
+                <ListItem size={6}>
+                  Achievements - This functionality is aimed at getting new
+                  users interested in learning to use vim
+                </ListItem>
+              </List>
+            </Fill>
+          </Layout>
         </Slide>
         <Slide>
-          <Heading>Where are we going?</Heading>
-          <Text
-            margin="10px 0 0"
-            size={6}
-            textColor="secondary"
-            textAlign="left"
-          >
+          <Heading size={5}>Where are we going?</Heading>
+          <Text margin="10px 0 0" size={6} textAlign="left">
             Performance has always been one of the major advantages of using
             vim, it has always had a very quick startup time and not been too
             memory intensive. We feel that whilst electron and typescript have
